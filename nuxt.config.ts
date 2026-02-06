b5ee75b9ba2aea60dd0e3bbadaef52d8eaaff176
Christian Huber <c.huber@iwf.ch>
2026-02-06 16:34:56 +0100

    init tailwindcss, rewrite to nuxt.js, add nuxtUI as design kit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+import { defineNuxtConfig } from "nuxt/config"
+
+export default defineNuxtConfig({
+  srcDir: "src",
+  css: ["@/styles/globals.css"],
+  modules: ["@pinia/nuxt", "@nuxt/ui"],
+  devtools: { enabled: true },
+  compatibilityDate: '2026-02-06'
+})
---
+++ src/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="space-y-4">
+    <h1 class="text-8xl font-semibold">You did it!</h1>
+    <UButton color="primary" variant="solid">Hello Nuxt UI</UButton>
+  </div>
+</template>
