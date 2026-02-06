97a16eb9da36cede046b20458cca9d093f4f6663
Christian Huber <c.huber@iwf.ch>
2026-02-06 20:43:40 +0100

    layout init, wrapping classes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from "nuxt/config"
 
 export default defineNuxtConfig({
   srcDir: "src",
-  css: ["@/styles/globals.css"],
+  css: ["@/styles/global.css"],
   modules: ["@pinia/nuxt", "@nuxt/ui", "@nuxt/content"],
   devtools: { enabled: true },
   compatibilityDate: '2026-02-06'
